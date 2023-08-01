19401fc54527408ca9514b265fbc5b1c234f1069
Jaehong Cho <83823618+JaehongCS20@users.noreply.github.com>
2023-08-02 01:33:11 +0900

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,6 @@
-# Build in Docker because of compability issue
+# Build in Docker because of compatibility issue
 # Follow instructions in https://github.com/NVIDIA/FasterTransformer/blob/main/docs/gpt_guide.md
 # Optimized code will be also located outside of docker
+# `pwd` is the current directory
+# cudann installed here
 nvidia-docker run -ti --shm-size 5g -v `pwd`:/workspace nvcr.io/nvidia/pytorch:22.09-py3 bash
